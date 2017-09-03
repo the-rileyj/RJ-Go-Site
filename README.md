bfa5c0134e92b0eddcdf7562f88b7211df6df5ef
Riley <rileyjohnson99@hotmail.com>
2017-09-03 15:18:27 -0500

    'Initialization'

---
+++ README.md
@@ -0,0 +1,3 @@
+# RJ's New Site 
+ 
+RJ's new site, where the backend is implemented in GoLang 
---
+++ index.go
@@ -0,0 +1,160 @@
+package main
+
+import(
+	"net/http"
+	"math/rand"
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"time"
+	"strings"
+	"net"
+	"bytes"
+)
+
+//ipRange - a structure that holds the start and end of a range of ip addresses
+type ipRange struct {
+	start net.IP
+	end net.IP
+}
+
+var privateRanges = []ipRange{
+	ipRange{
+		start: net.ParseIP("10.0.0.0"),
+		end:   net.ParseIP("10.255.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("100.64.0.0"),
+		end:   net.ParseIP("100.127.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("172.16.0.0"),
+		end:   net.ParseIP("172.31.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.0.0.0"),
+		end:   net.ParseIP("192.0.0.255"),
+	},
+	ipRange{
+		start: net.ParseIP("192.168.0.0"),
+		end:   net.ParseIP("192.168.255.255"),
+	},
+	ipRange{
+		start: net.ParseIP("198.18.0.0"),
+		end:   net.ParseIP("198.19.255.255"),
+	},
+}
+
+type visiTracker struct {
+	V      int `json:"numb"`
+	Uv     int `json:"uniq"`
+	IpList []string `json:"ips"`
+}
+
+var tpl *template.Template
+var vT visiTracker
+
+// inRange - check to see if a given ip address is within a range given
+func inRange(r ipRange, ipAddress net.IP) bool {
+	// strcmp type byte comparison
+	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+		return true
+	}
+	return false
+}
+
+func (vT *visiTracker) InSlice(a string) bool {
+	for _, b := range vT.IpList {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	fi, err := ioutil.ReadFile("../numer.json")
+	if err == nil {
+		json.Unmarshal(fi, &vT)
+	} else {
+		print("ERRRRRRRRRRR")
+		os.Exit(1)
+	}
+	tpl = template.New("").Funcs(template.FuncMap{"snapcode":getSnap})
+	tpl.ParseGlob("templates/*.gohtml")
+}
+
+func getSnap() string{
+	if rand.Intn(2) == 1{
+		return "snapcode_cash"
+	} else {
+		return "snapcode_casher"
+	}
+}
+
+// isPrivateSubnet - check to see if this ip is in a private subnet
+func isPrivateSubnet(ipAddress net.IP) bool {
+	// my use case is only concerned with ipv4 atm
+	if ipCheck := ipAddress.To4(); ipCheck != nil {
+		// iterate over all our ranges
+		for _, r := range privateRanges {
+			// check if this ip is in a private range
+			if inRange(r, ipAddress){
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func getIPAdress(r *http.Request) string {
+	for _, h := range []string{"X-Forwarded-For", "X-Real-Ip"} {
+		addresses := strings.Split(r.Header.Get(h), ",")
+		// march from right to left until we get a public address
+		// that will be the address right before our proxy.
+		for i := len(addresses) -1 ; i >= 0; i-- {
+			ip := strings.TrimSpace(addresses[i])
+			// header can contain spaces too, strip those out.
+			realIP := net.ParseIP(ip)
+			if !realIP.IsGlobalUnicast() || isPrivateSubnet(realIP) {
+				// bad address, go to next
+				continue
+			}
+			return ip
+		}
+	}
+	return ""
+}
+
+func structToJson(strct interface{}){
+	res, err := json.Marshal(strct)
+	if err != nil {
+		println(err)
+		return
+	}
+	err = ioutil.WriteFile("../numer.json", res, 0644)
+}
+
+func index(w http.ResponseWriter, r *http.Request){
+	if r.URL.Query()["check"] == nil{
+		vT.V++
+		if getIPAdress(r) != "" && !vT.InSlice(getIPAdress(r)){
+			vT.Uv++
+			vT.IpList = append(vT.IpList, r.Header.Get("X-Forwarded-For"))
+		}
+		go structToJson(vT)
+	}
+
+	err := tpl.ExecuteTemplate(w, "index.gohtml", vT)
+	if err != nil{
+		print(err)
+	}
+}
+
+func main(){
+	http.HandleFunc("/", index)
+	http.Handle("/public/", http.FileServer(http.Dir("static/")))
+	http.ListenAndServe(":3030", nil)
+}
